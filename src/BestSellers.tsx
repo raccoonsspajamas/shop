c972b5ce2bc8c8b2e5185431ee5fc9d4c9f7e1ad
raccoonsspajamas <zxcodingninja@gmail.com>
2025-04-18 00:21:45 +0200

    server requset

--- src/BestSellers.tsx
+++ src/BestSellers.tsx
@@ -1,60 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
 
 export const BestSellers = () => {
 
-    const products = [
-        {
-            _id: "664476983793ba7564c50ba2",
-            id: 8,
-            title: "Zip hoodie",
-            price: 91.99,
-            description: "Этот зип-худи, изготовленный из мягкого и уютного материала, предлагает идеальное сочетание стиля и комфорта для прохладной погоды. Его удобный крой с молнией спереди и капюшоном позволяет легко регулировать тепло, делая его идеальным выбором для повседневного ношения или для слоя одежды во время активных занятий на открытом воздухе.",
-            category: "men's clothing",
-            image: "https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/e12cebe8-7d13-4842-98ab-f71e0df6a2f4_8-zip-hoodie.png",
-            rating: {
-                rate: 4.7,
-                count: 120
-            },
-            createdAt: "2024-05-15T08:47:20.180Z",
-            updatedAt: "2024-05-15T12:20:34.117Z",
-            __v: 0
-        },
-        {
-            _id: "664476983793ba7564c50ba2",
-            id: 8,
-            title: "Zip hoodie",
-            price: 92.99,
-            description: "Этот зип-худи, изготовленный из мягкого и уютного материала, предлагает идеальное сочетание стиля и комфорта для прохладной погоды. Его удобный крой с молнией спереди и капюшоном позволяет легко регулировать тепло, делая его идеальным выбором для повседневного ношения или для слоя одежды во время активных занятий на открытом воздухе.",
-            category: "men's clothing",
-            image: "https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/e12cebe8-7d13-4842-98ab-f71e0df6a2f4_8-zip-hoodie.png",
-            rating: {
-                rate: 4.7,
-                count: 120
-            },
-            createdAt: "2024-05-15T08:47:20.180Z",
-            updatedAt: "2024-05-15T12:20:34.117Z",
-            __v: 0
-        }
-    ];
+    const [products, setProducts] = useState<any>([])
+
+    useEffect(() => {
+        //обращение к серверу при загрузке стр
+        const promise = axios.get("https://masterclass.kimitsu.it-incubator.io/api/products")
+        promise.then((res) => {
+            const products = res.data;
+            setProducts(products)
+        });
+    }, []);
+
+
+
+
+
 
     return (
+        <div className='bestSeller'>
+            <h2>Bestsellers</h2>
+            <div className="cards">
 
-        <div className="cards">
-
-            {products.map((product) => {
-                // debugger
-                return (
-                    <div className="card">
-                        <img src={product.image} alt="img" />
-                        <h4>{product.title}</h4>
-                        <p className="price">${product.price}</p>
-                        <button>Show more</button>
-                    </div>
-                )
-            })}
+                {products.map((product) => {
+                    // debugger
+                    return (
+                        <div className="card">
+                            <img src={product.image} alt="img" />
+                            <h4>{product.title}</h4>
+                            <p className="price">${product.price}</p>
+                            <button>Show more</button>
+                        </div>
+                    )
+                })}
 
 
+            </div>
         </div>
-
     );
 };
